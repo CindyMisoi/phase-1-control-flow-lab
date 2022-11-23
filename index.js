f35190cc0a433abb7b7aa2f4bec18628931d1657
CindyMisoi <crmisoi2k@gmail.com>
2022-11-23 09:00:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(somevalue){
+  if(somevalue <= 400){
+    return `This one is on me!`;
+  }
+  else if(somevalue < 2500 && somevalue > 2000){
+    return `I will gladly take your thirty bucks.`;
+  }
+  else if(somevalue > 2000 ){
+    return `No can do.`;
+  }
 }
+scuberGreetingForFeet(199);
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === 'NYC'){
+    return `Ok, sounds good.`;
+  }
+  else{
+    return `No go.`;
+  }
 }
+ternaryCheckCity("NYC");
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === "generous"){
+    return 'Thank you so much.';
+  }
+  else if(tip === "not as generous"){
+    return 'Thank you.';
+  }
+  else{
+    return 'Bye.';
+  }
 }
+switchOnCharmFromTip("generous");
